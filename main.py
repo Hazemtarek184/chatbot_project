d25a83e2e7199d19928e31bb1f4b3e83f679fa14
Hazemtarek184 <hazemtarek184@gmail.com>
2025-06-24 01:39:58 +0300

    fixing port error

--- main.py
+++ main.py
@@ -151,5 +151,4 @@ async def welcome():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
